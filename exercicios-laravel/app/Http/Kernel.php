ae85f5cc11155a9c0f4f5a14469a90599ed67afe
Gustavo <gustavo.sousa@soluti.com.br>
2022-01-05 12:54:33 -0300

    configura jwt-auth

--- exercicios-laravel/app/Http/Kernel.php
+++ exercicios-laravel/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'verificasessao' => \App\Http\Middleware\VerificaSessao::class,
+        'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
     ];
 }
